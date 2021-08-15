d64137c8db4db51d88de14f8dbaa5de20a88f32c
Ralph Lawrence S. Bedayo <ralphbedayo009@gmail.com>
2021-08-15 13:45:06 +0800

    Modulized search component

---
+++ resources/js/src/components/common/Search.vue
@@ -0,0 +1,173 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="w-75  shadow p-3 rounded rounded-3">
+            <form action="#" v-on:submit.prevent="emitSearch">
+                <h3>Search and Sort</h3>
+                <div class="row">
+                    <div class="row">
+                        <div class="d-flex justify-content-between">
+                            <div class="input-group mb-3" style="width: 45%;">
+                                <span class="input-group-text">Search By</span>
+                                <select class="form-select" id="searchFieldOptions"
+                                        v-model="sSearchField">
+                                    <option disabled value="">Select Field</option>
+                                    <option :value="sSearchFieldOriginal"
+                                            v-for="(sSearchFieldAlias, sSearchFieldOriginal) in this.search_field_options">
+                                        {{sSearchFieldAlias}}
+                                    </option>
+                                </select>
+                            </div>
+                            <div class="input-group mb-3" style="width: 45%;">
+                                <input type="text" class="form-control" placeholder="Search"
+                                       aria-label="Search"
+                                       v-model="sSearchValue"
+                                       aria-describedby="basic-addon2">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="d-flex justify-content-between">
+                            <div class="input-group mb-3" style="width: 45%;">
+                                <span class="input-group-text">Order By</span>
+                                <select class="form-select" id="orderByOptions"
+                                        v-model="sOrderByValue">
+                                    <option disabled value="">Select Order</option>
+                                    <option :value="sOrderByField"
+                                            v-for="(sOrderByAlias, sOrderByField) in this.order_by_options">
+                                        {{sOrderByAlias}}
+                                    </option>
+                                </select>
+                            </div>
+
+                            <div class="input-group mb-3" style="width: 45%;">
+                                <span class="input-group-text">Sort By</span>
+                                <select class="form-select" id="sortByOptions"
+                                        v-model="sSortByValue">
+                                    <option disabled value="">Select Sort Field</option>
+                                    <option :value="sSortByField"
+                                            v-for="(sSortByAlias, sSortByField) in this.oSortByOptions">
+                                        {{sSortByAlias}}
+                                    </option>
+                                </select>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="row" v-if="is_blog_home">
+                        <div class="row">
+                            <div class="col-2">
+                                <div class="row mb-2">
+                                    <strong>Categories: </strong>
+                                </div>
+                                <div class="row px-2 mb-2">
+                                    <button type="button" class="btn btn-sm btn-outline-primary"
+                                            v-on:click="checkAllCategory">Check All
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="col-10">
+                                <div class="row">
+                                    <div class="form-check col-3"
+                                         v-for="(oCategory, mKey) in categories">
+                                        <input class="form-check-input" type="checkbox"
+                                               :id="'cb_' + oCategory.title" :value="oCategory.id"
+                                               v-model="oCategoriesCheckedValue[oCategory.id]"
+                                        >
+                                        <label class="form-check-label" :for="'cb_' + oCategory.title">{{_.upperFirst(oCategory.title)}}</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn btn-success w-25"> Go</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</template>
+<script>
+    export default {
+        name: "Search",
+        props: [
+            'search_field_options',
+            'order_by_options',
+            'is_blog_home',
+            'default_search_field',
+            'default_order_value',
+            'default_sort_value',
+            'categories'
+        ],
+        data() {
+            return {
+                oSortByOptions: {
+                    'asc': 'Ascending',
+                    'desc': 'Descending'
+                },
+                sSearchField: this.default_search_field ?? '',
+                sSearchValue: '',
+                sSortByValue: this.default_sort_value ?? 'desc',
+                sOrderByValue: this.default_order_value ?? 'created_at',
+                oCategories: this.categories,
+                oCategoriesCheckedValue: {
+                    1: true,
+                    2: true,
+                    3: true,
+                    4: true,
+                    5: true,
+                    6: true,
+                    7: true,
+                    8: true,
+                    9: true,
+                    10: true,
+                }
+            };
+        },
+        methods: {
+            emitSearch() {
+                let sL5SearchString = this.setSearchFieldParam();
+                sL5SearchString += this.setCategorySearchParam();
+                let oSortAndOrderParam = this.setSortAndOrderParam();
+
+                let oSearchValues = {
+                    search: sL5SearchString, ...oSortAndOrderParam
+                };
+
+                this.$emit('input', oSearchValues);
+            },
+            checkAllCategory() {
+                for (const [mKey, oCategory] of Object.entries(this.categories)) {
+                    this.oCategoriesCheckedValue[oCategory.id] = true;
+                }
+            },
+            setSearchFieldParam() {
+                if (this.sSearchField.length === 0 || this.sSearchValue.length === 0) {
+                    return '';
+                }
+                return this.sSearchField + ':' + this.sSearchValue + ';';
+            },
+            setSortAndOrderParam() {
+                return {sortedBy: this.sSortByValue, orderBy: this.sOrderByValue};
+            },
+            setCategorySearchParam() {
+                let sCategoryIdSearch = '';
+
+                for (const [sCategoryId, bChecked] of Object.entries(this.oCategoriesCheckedValue)) {
+                    if (bChecked) {
+                        sCategoryIdSearch += sCategoryId + ',';
+                    }
+                }
+
+                return 'category_id:' + sCategoryIdSearch.slice(0, -1);
+            },
+        }
+    };
+
+</script>
+
+<style scoped></style>
--- resources/js/src/pages/Home.vue
+++ resources/js/src/pages/Home.vue
@@ -14,101 +14,27 @@
                 </div>
 
                 <div class="row mt-5">
-                    <div class="d-flex justify-content-center">
-                        <div class="w-75  shadow p-3 rounded rounded-3">
-                            <form action="#" v-on:submit.prevent="search">
-                                <h3>Search and Sort</h3>
-                                <div class="row">
-                                    <div class="row">
-                                        <div class="d-flex justify-content-between">
-                                            <div class="input-group mb-3" style="width: 45%;">
-                                                <span class="input-group-text">Search By</span>
-                                                <select class="form-select" id="searchFieldOptions"
-                                                        v-model="sSearchField">
-                                                    <option disabled value="">Select Field</option>
-                                                    <option :value="sSearchFieldOriginal"
-                                                            v-for="(sSearchFieldAlias, sSearchFieldOriginal) in oSearchFieldOptions">
-                                                        {{sSearchFieldAlias}}
-                                                    </option>
-                                                </select>
-                                            </div>
-                                            <div class="input-group mb-3" style="width: 45%;">
-                                                <input type="text" class="form-control" placeholder="Search"
-                                                       aria-label="Search"
-                                                       v-model="sSearchValue"
-                                                       aria-describedby="basic-addon2">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="d-flex justify-content-between">
-                                            <div class="input-group mb-3" style="width: 45%;">
-                                                <span class="input-group-text">Order By</span>
-                                                <select class="form-select" id="orderByOptions" v-model="sOrderByValue">
-                                                    <option disabled value="">Select Order</option>
-                                                    <option :value="sOrderByField"
-                                                            v-for="(sOrderByAlias, sOrderByField) in oOrderByOptions">
-                                                        {{sOrderByAlias}}
-                                                    </option>
-                                                </select>
-                                            </div>
-
-                                            <div class="input-group mb-3" style="width: 45%;">
-                                                <span class="input-group-text">Sort By</span>
-                                                <select class="form-select" id="sortByOptions" v-model="sSortByValue">
-                                                    <option disabled value="">Select Sort Field</option>
-                                                    <option :value="sSortByField"
-                                                            v-for="(sSortByAlias, sSortByField) in oSortByOptions">
-                                                        {{sSortByAlias}}
-                                                    </option>
-                                                </select>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="row">
-                                            <div class="col-2">
-                                                <div class="row mb-2">
-                                                    <strong>Categories: </strong>
-                                                </div>
-                                                <div class="row px-2 mb-2">
-                                                    <button type="button" class="btn btn-sm btn-outline-primary"
-                                                            v-on:click="checkAllCategory">Check All
-                                                    </button>
-                                                </div>
-                                            </div>
-                                            <div class="col-10">
-                                                <div class="row">
-                                                    <div class="form-check col-3"
-                                                         v-for="(oCategory, mKey) in oCategories">
-                                                        <input class="form-check-input" type="checkbox"
-                                                               :id="'cb_' + oCategory.title" :value="oCategory.id"
-                                                               v-model="oCategoriesCheckedValue[oCategory.id]"
-                                                        >
-                                                        <label class="form-check-label" :for="'cb_' + oCategory.title">{{_.upperFirst(oCategory.title)}}</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row mt-3">
-                                    <div class="d-flex justify-content-center">
-                                        <button type="submit" class="btn btn-success w-25"> Go</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                    <search
+                        is_blog_home="true"
+                        default_search_field="title"
+                        default_order_value="created_at"
+                        default_sort_value="desc"
+                        :search_field_options="oSearchFieldOptions"
+                        :order_by_options="oOrderByOptions"
+                        :categories="oCategories"
+                        v-model="oSearchParams"
+                    ></search>
                 </div>
                 <div class="row mt-5">
                     <div class="d-flex justify-content-end">
                         <div>
-                            <button class="btn btn-outline-primary" v-on:click="prevPage" :disabled="this.iPage === 1"> <</button>
+                            <button class="btn btn-outline-primary" v-on:click="prevPage" :disabled="this.iPage === 1">
+                                <
+                            </button>
                             <span class="mx-1">Page {{ this.iPage}} out of {{ this.iTotalPage}}</span>
-                            <button class="btn btn-outline-primary" v-on:click="nextPage" :disabled="this.iPage === this.iTotalPage"> ></button>
+                            <button class="btn btn-outline-primary" v-on:click="nextPage"
+                                    :disabled="this.iPage === this.iTotalPage"> >
+                            </button>
                         </div>
 
                     </div>
@@ -135,10 +61,11 @@
     import BlogItem from "../components/home/BlogItem";
     import Blog from "../models/Blog";
     import Category from "../models/Category";
+    import Search from "../components/common/Search";
 
     export default {
         name: "Home",
-        components: {BlogItem, Navbar},
+        components: {Search, BlogItem, Navbar},
         data() {
             return {
                 oUser: User,
@@ -163,65 +90,23 @@
                 oSortByOptions: {
                     'asc': 'Ascending',
                     'desc': 'Descending'
-                },
-                sSearchField: 'title',
-                sSearchValue: '',
-                sSortByValue: 'desc',
-                sOrderByValue: 'created_at',
-                oCategoriesCheckedValue: {
-                    1: true,
-                    2: true,
-                    3: true,
-                    4: true,
-                    5: true,
-                    6: true,
-                    7: true,
-                    8: true,
-                    9: true,
-                    10: true,
                 }
             }
         },
+        watch: {
+            oSearchParams: function (val) {
+                this.search();
+            }
+        },
         methods: {
-            setSearchParams() {
-                this.oSearchParams['limit'] = this.iLimit;
-                this.oSearchParams['page'] = this.iPage;
-            },
-            checkAllCategory() {
-                for (const [mKey, oCategory] of Object.entries(this.oCategories)) {
-                    this.oCategoriesCheckedValue[oCategory.id] = true;
-                }
-            },
             async search() {
-                let sL5SearchString = this.setSearchFieldParam();
-                let oSortAndOrderParam = this.setSortAndOrderParam();
                 let oPagination = {page: this.iPage, limit: this.iLimit};
-                sL5SearchString += this.setCategorySearchParam();
+                let oSearchParams = this.oSearchParams;
 
-                let oResponse = await Blog.fetch({search: sL5SearchString, ...oSortAndOrderParam, ...oPagination});
+                let oResponse = await Blog.fetch({...oSearchParams, ...oPagination});
                 this.oBlogItems = oResponse.data;
                 this.iPage = oResponse.meta.pagination.current_page;
                 this.iTotalPage = oResponse.meta.pagination.total_pages;
-            },
-            setSearchFieldParam() {
-                if (this.sSearchField.length === 0 || this.sSearchValue.length === 0) {
-                    return '';
-                }
-                return this.sSearchField + ':' + this.sSearchValue + ';';
-            },
-            setSortAndOrderParam() {
-                return {sortedBy: this.sSortByValue, orderBy: this.sOrderByValue};
-            },
-            setCategorySearchParam() {
-                let sCategoryIdSearch = '';
-
-                for (const [sCategoryId, bChecked] of Object.entries(this.oCategoriesCheckedValue)) {
-                    if (bChecked) {
-                        sCategoryIdSearch += sCategoryId + ',';
-                    }
-                }
-
-                return 'category_id:' + sCategoryIdSearch.slice(0, -1);
             },
             prevPage() {
                 this.iPage -= 1;
@@ -242,10 +127,8 @@
             }
         },
         async beforeMount() {
-            this.setSearchParams();
             this.oCategories = await Category.fetchAll();
             this.search();
-            this.checkAllCategory();
         }
     }
 </script>
